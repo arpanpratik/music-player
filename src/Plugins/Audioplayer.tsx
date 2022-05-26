c50fa5d23546c254f1d84086f7ef8d6ace4ac269
arpanpratik <arpanpratik@gmail.com>
2022-05-26 13:07:48 +0530

    Added audio player

--- src/Plugins/Audioplayer.tsx
+++ src/Plugins/Audioplayer.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const Audioplayer = () => {
+  const mp3 = 'https://aveclagare.org/mp3/One%20Shot%20Lili%20-%20Master%20Half%20Wizard.mp3';
+  // let audio = new Audio();
+  let src;
+  // audio.play();
+
+  // console.log('audio', audio);
+
+  const play = () => {
+    src = mp3;
+
+    console.log('clicked');
+    
+    // document.getElementById('my-player').play()
+
+    // audio.play();
+  };
+  // const stop = () => audio.pause();
+
   return (
-    <div>Audioplayer</div>
+    <div>
+      Audioplayer <br />
+
+      <audio className="my_audio" controls preload="none" id='my-player'>
+        <source src="{src}" type="audio/mpeg" />
+        <source src="{src}" type="audio/ogg" />
+      </audio>
+
+      <button type="button" onClick={play}>Play</button>
+    </div>
   )
 }
 
-export default Audioplayer
+export default Audioplayer;
